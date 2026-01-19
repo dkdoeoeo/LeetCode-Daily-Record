aef9e0001e87d2aaf8d3b5606c4c8c687c8f754f
dkdoeoeo <130052795+dkdoeoeo@users.noreply.github.com>
2026-01-19 21:30:36 +0800

    Time: 174 ms (12.5%), Space: 17.5 MB (75%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.c
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.c
@@ -1,44 +1,54 @@
-
-int min(int a, int b) {
-    return a < b ? a : b;
-}
-
-int getRectSum(int** sum, int r1, int c1, int r2, int c2) {
-    return sum[r2][c2] - sum[r1-1][c2] - sum[r2][c1-1] + sum[r1-1][c1-1];
+int max(int a, int b){
+    if(a>b){
+        return a;
+    }
+    return b;
 }
 
 int maxSideLength(int** mat, int matSize, int* matColSize, int threshold) {
-    int m = matSize;
-    int n = *matColSize;
-    
-    int** p = (int**)malloc(sizeof(int*) * (m + 1));
-    for(int i = 0; i <= m; i++) {
-        p[i] = (int*)calloc(n + 1, sizeof(int));
-    }
-    
-    for(int i = 1; i <= m; i++) {
-        for(int j = 1; j <= n; j++) {
-            p[i][j] = mat[i-1][j-1] + p[i-1][j] + p[i][j-1] - p[i-1][j-1];
+    int** PF = (int **) malloc(matSize*sizeof(int*));
+    for(int i = 0; i < matSize;i++) PF[i] = (int*)malloc(sizeof(int)*(*matColSize));
+
+    for(int i = 0; i < matSize;i++){
+        for(int j = 0; j < * matColSize;j++){
+            if(i>0 && j>0){
+                PF[i][j] = mat[i][j] + PF[i-1][j] + PF[i][j-1] - PF[i-1][j-1];
+            }
+            else if(i == 0 && j > 0){
+                PF[i][j] = mat[i][j] + PF[i][j-1];
+            }
+            else if(j == 0 && i > 0){
+                PF[i][j] = mat[i][j] + PF[i-1][j];
+            }
+            else{
+                PF[i][j] = mat[i][j];
+            }
         }
     }
-    
-    int ans = 0;
-    
-    for(int i = 1; i <= m; i++) {
-        for(int j = 1; j <= n; j++) {
-            int currentLen = ans + 1;
-            
-            if (i >= currentLen && j >= currentLen) {
-                int r1 = i - currentLen + 1;
-                int c1 = j - currentLen + 1;
-                int total = getRectSum(p, r1, c1, i, j);
 
-                if (total <= threshold) {
-                    ans++;
+    int ans = max(matSize,* matColSize);
+    for(int size = ans;size>0;size--){
+        for(int i = 0; i <= matSize - size;i++){
+            for(int j = 0; j <= * matColSize - size;j++){
+                int temp = 1e5;
+                if(i>0 && j>0){
+                    temp = PF[i-1][j-1] + PF[i+size - 1][j+size-1] - PF[i - 1][j+size-1] - PF[i+size - 1][j-1];
+                }
+                else if(j==0 && i > 0){
+                    temp = PF[i+size-1][j+size-1] - PF[i-1][j+size-1];
+                }
+                else if(i == 0 && j > 0){
+                    temp = PF[i+size-1][j+size-1] - PF[i+size-1][j-1];
+                }
+                else{
+                    temp = PF[i+size - 1][j+size-1];
+                }
+
+                if(threshold >= temp){
+                    return size;
                 }
             }
         }
     }
-    
-    return ans;
+    return 0;
 }
