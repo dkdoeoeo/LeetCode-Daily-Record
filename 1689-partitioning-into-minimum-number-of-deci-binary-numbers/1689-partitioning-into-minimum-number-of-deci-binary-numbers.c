d8e9cbcf76c6a16582236dd7c43c9216c2947e26
dkdoeoeo <130052795+dkdoeoeo@users.noreply.github.com>
2026-03-01 12:08:32 +0800

    Time: 0 ms (100%), Space: 11.1 MB (1.89%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.c
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -1,8 +1,14 @@
 int minPartitions(char* n) {
     int ans = 0;
-    for(int i = 0; i < strlen(n);i++){
-        if(n[i] - '0' > ans){
-            ans = n[i] - '0';
+    for(int i = 0; n[i] != '\0';i++){
+        int current_digit = n[i] - '0';
+        
+        if (current_digit > ans) {
+            ans = current_digit;
+
+            if (ans == 9) {
+                return 9;
+            }
         }
     }
     return ans;
