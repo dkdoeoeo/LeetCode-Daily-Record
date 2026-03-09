d3bbddeace7db1772106f65af556354f96773132
dkdoeoeo <130052795+dkdoeoeo@users.noreply.github.com>
2026-03-09 20:11:46 +0800

    Time: 18 ms (0%), Space: 9.7 MB (25%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.c
@@ -0,0 +1,29 @@
+int numberOfStableArrays(int zero, int one, int limit) {
+    long long MOD = 1000000007;
+    long long dp[205][205][2] = {0};
+
+    for (int i = 1; i <= zero && i <= limit; i++) {
+        dp[i][0][0] = 1;
+    }
+    for (int j = 1; j <= one && j <= limit; j++) {
+        dp[0][j][1] = 1;
+    }
+
+    for (int i = 1; i <= zero; i++) {
+        for (int j = 1; j <= one; j++) {
+            
+            dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1]) % MOD;
+
+            if (i > limit) {
+                dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+            }
+
+            dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1]) % MOD;
+            if (j > limit) {
+                dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+            }
+        }
+    }
+
+    return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+}
